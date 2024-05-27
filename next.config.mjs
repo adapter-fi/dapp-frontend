a0afedb0b731593b364057515db88c7e509a772a
swheel <swheel33@gmail.com>
2024-05-27 13:49:18 -0400

    feat: rewrites for posthog

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,20 @@ const nextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding')
     return config
   },
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  // This is required to support PostHog trailing slash API requests
+  skipTrailingSlashRedirect: true, 
 }
 
 export default nextConfig
