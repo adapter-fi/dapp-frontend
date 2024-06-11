45fbaa27b6d0e422c2ed1b81194267508cb0d849
swheel <swheel33@gmail.com>
2024-06-11 13:42:15 -0400

    feat: landing setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,14 +34,21 @@ export default function Home() {
               <p className="text-[#125AFA]">MAXIMIZED</p>
             </div>
             <div className="grid grid-cols-2 gap-2">
-              {/* <Link href="/vaults">
-                <Button className="w-full"
-                 variant="inverted" 
-                >
+              <Link href="/vaults">
+                <Button className="w-full" variant="inverted">
                   LAUNCH APP
                 </Button>
-              </Link> */}
-              <Button variant="secondary">READ DOCS</Button>
+              </Link>
+              <a
+                href={docsUrl}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="w-full"
+              >
+                <Button variant="secondary" className="w-full">
+                  READ DOCS
+                </Button>
+              </a>
             </div>
           </div>
         </div>
@@ -87,10 +94,9 @@ export default function Home() {
                 <p>Get an on-chain butler</p>
               </div>
             </div>
-            <Button className="w-[300px] mt-4" disabled>
-              {/* Compound Now */}
-              COMING SOON
-            </Button>
+            <Link href="/vaults">
+              <Button className="w-[300px] mt-4">COMPOUND NOW</Button>
+            </Link>
           </div>
           <div className="w-screen bg-[#125AFA] overflow-clip relative h-[480px]">
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[416px] h-[506px]">
@@ -171,12 +177,17 @@ export default function Home() {
               <p className="text-[#125AFA]">MAXIMIZED</p>
             </div>
             <div className="grid grid-cols-2 gap-2">
-              {/* <Link href="/vaults"> */}
-              <Button className="w-full h-16" variant="inverted" disabled>
-                LAUNCH APP
-              </Button>
-              {/* </Link> */}
-              <a href={docsUrl} target="_blank" rel="noreferrer noopener" className='w-full'>
+              <Link href="/vaults">
+                <Button className="w-full h-16" variant="inverted">
+                  LAUNCH APP
+                </Button>
+              </Link>
+              <a
+                href={docsUrl}
+                target="_blank"
+                rel="noreferrer noopener"
+                className="w-full"
+              >
                 <Button variant="secondary" className="h-16 w-full">
                   READ DOCS
                 </Button>
@@ -184,14 +195,14 @@ export default function Home() {
             </div>
           </div>
         </div>
-        {/* <div
+        <div
           className={cn(
             'sticky top-0 z-10 text-[#F9F9F2] bg-[#0B0B0A] snap-align-none',
             inViewport && 'hidden'
           )}
         >
           <Navbar />
-        </div> */}
+        </div>
         <div className="min-h-screen snap-center snap-always flex px-12 bg-[#0B0B0A]">
           <div className="flex flex-col gap-2 min-w-[662px] justify-end pb-[118px]">
             <div className="flex flex-col leading-[0.8] text-white font-bold text-[56px]">
@@ -234,9 +245,9 @@ export default function Home() {
                 <p>Get an on-chain butler</p>
               </div>
             </div>
-            <Button className="w-[300px] h-16" disabled>
-              COMING SOON
-            </Button>
+            <Link href="/vaults">
+              <Button className="w-[300px] h-16">COMPOUND NOW</Button>
+            </Link>
           </div>
           <div className="w-[802px] bg-[#125AFA] overflow-clip relative h-screen">
             <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[495px] h-[3033px]">
