2c41ba7ff6622182a64ae0310e3849c6ce1b917b
swheel <swheel33@gmail.com>
2024-06-25 10:37:42 -0400

    feat: only show deprecated vaults if you have a balance

--- src/components/VaultsChart.tsx
+++ src/components/VaultsChart.tsx
@@ -7,10 +7,10 @@ import { Skeleton } from '@/components/ui/skeleton'
 import { useVaultBalances } from '@/hooks/use-vault-balances'
 
 import { getPendleMarketData } from '@/lib/queries/get-pendle-market-data'
+import { formatPercentage } from '@/lib/utils'
 
 import { columns } from '@/app/(dapp)/vaults/columns'
 import { DataTable } from '@/app/(dapp)/vaults/data-table'
-import { formatPercentage } from '@/lib/utils'
 
 export const VaultsChart = () => {
   const { data: pendleData } = useQuery({
@@ -22,7 +22,16 @@ export const VaultsChart = () => {
 
   const chartData = pendleData?.map(
     (
-      { logoURI, protocolURI, name, underlyingApy, impliedApy, type, chain, deprecated },
+      {
+        logoURI,
+        protocolURI,
+        name,
+        underlyingApy,
+        impliedApy,
+        type,
+        chain,
+        deprecated,
+      },
       i
     ) => ({
       data: {
@@ -32,18 +41,28 @@ export const VaultsChart = () => {
         type,
         deprecated,
       },
-      underlyingAPR: underlyingApy !== undefined ? formatPercentage(underlyingApy * 100) : 'TBD',
-      autocompoundedAPY: underlyingApy !== undefined ? formatPercentage(impliedApy * 100) : 'TBD',
+      underlyingAPR:
+        underlyingApy !== undefined
+          ? formatPercentage(underlyingApy * 100)
+          : 'TBD',
+      autocompoundedAPY:
+        underlyingApy !== undefined
+          ? formatPercentage(impliedApy * 100)
+          : 'TBD',
       deposits: vaultBalances?.at(i)?.tvl || 0,
       holdings: vaultBalances?.at(i)?.balance || 0,
       network: chain.name,
     })
   )
 
+  const filteredChartData = chartData?.filter(
+    ({ data: { deprecated }, holdings }) => !deprecated || (deprecated && holdings > 0)
+  )
+
   return (
     <div className="p-12">
       {chartData ? (
-        <DataTable columns={columns} data={chartData} vaultTable />
+        <DataTable columns={columns} data={filteredChartData!} vaultTable />
       ) : (
         <Skeleton className="w-full h-[300px]" />
       )}
