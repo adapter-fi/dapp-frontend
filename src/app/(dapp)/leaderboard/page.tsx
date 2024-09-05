b273779048dfd54afc2e32d33af38648a1d928a1
swheel <swheel33@gmail.com>
2024-09-05 10:31:49 -0400

    feat: leaderboard initial setup

--- src/app/(dapp)/leaderboard/page.tsx
+++ src/app/(dapp)/leaderboard/page.tsx
@@ -1,42 +1,37 @@
+import { Suspense } from 'react'
+
 import { Metric } from '@/components/Metric'
 
-import { DataTable } from '@/app/(dapp)/vaults/data-table'
-import { Points, columns } from '@/app/(dapp)/leaderboard/columns'
+import { columns } from '@/app/(dapp)/leaderboard/columns'
+import { DataTable } from '@/app/(dapp)/leaderboard/data-table'
 
-const fakeData: Points[] = [
-  {
-    rank: 1,
-    name: '0xa85724f7122dE7AD30CEAB290109e961dA24F269',
-    value: 100000,
-    points: 100000,
-  },
-  {
-    rank: 2,
-    name: '0xa85724f7122dE7AD30CEAB290109e961dA24F269',
-    value: 100000,
-    points: 90000,
-  },
-  {
-    rank: 3,
-    name: '0xa85724f7122dE7AD30CEAB290109e961dA24F269',
-    value: 100000,
-    points: 80000,
-  },
-  {
-    rank: 4,
-    name: '0xa85724f7122dE7AD30CEAB290109e961dA24F269',
-    value: 100000,
-    points: 70000,
-  },
-  {
-    rank: 5,
-    name: '0xa85724f7122dE7AD30CEAB290109e961dA24F269',
-    value: 100000,
-    points: 60000,
-  },
-]
 
-export default function LeaderboardPage() {
+export default async function LeaderboardPage() {
+  const leaderboard = await fetch(
+    'https://adapterfi.index.biggestlab.io/leaderboard/',
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.ADAPTER_API_KEY}`,
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+    }
+  )
+    .then((res) => res.json())
+    .then((data) =>
+      data.map(
+        ({
+          points,
+          address,
+          rank,
+        }: {
+          points: number
+          address: string
+          rank: number
+        }) => ({ points, name: address, rank })
+      )
+    )
+
   return (
     <div className="flex flex-col">
       <div className="bg-[#125AFA] border border-[#0E47C5] px-12 pt-12 pb-4 flex flex-col justify-between">
@@ -58,9 +53,11 @@ export default function LeaderboardPage() {
         Your points are accumulated and tracked from Day 1, leaderboard coming
         soon.
       </p>
-      {/* <div className="p-12">
-        <DataTable columns={columns} data={fakeData} />
-      </div> */}
+      <div className="p-12">
+        <Suspense>
+          <DataTable columns={columns} data={leaderboard} />
+        </Suspense>
+      </div>
     </div>
   )
 }
